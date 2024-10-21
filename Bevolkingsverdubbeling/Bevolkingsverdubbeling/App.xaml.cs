4f2b1261e61592c06ebe84aaab5c7ce6980e92bc
Wannes Pipien <wannes.pipien@student.pxl.be>
2024-10-21 21:22:33 +0200

    Oefening af

---
+++ Bevolkingsverdubbeling/Bevolkingsverdubbeling/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Bevolkingsverdubbeling
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Bevolkingsverdubbeling/Bevolkingsverdubbeling/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bevolkingsverdubbeling
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool validGrowth = double.TryParse(growthTextBox.Text, out double growth);
+            bool validPopulation = double.TryParse(populationtextBox.Text, out double population);
+            double doublePopulation = population * 2;
+            double growthPercentage = (1 + (growth / 100));
+            int years = 1;
+            if(growth == 0)
+            {
+                MessageBox.Show("Zonder groeipercentage is er geen verdubbeling.");
+            }
+            else
+            {
+                do
+                {
+                    population *= growthPercentage;
+                    years++;
+                } while (population < doublePopulation);
+                resultTextBox.Text = $"Nieuw bevolkingsgetal na {years} jaren is {population} mensen. ";
+            }
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox.Clear();
+            populationtextBox.Clear();
+            growthTextBox.Clear();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
